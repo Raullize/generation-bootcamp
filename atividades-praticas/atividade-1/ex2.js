0bc91c3e290a4965b3e579287ec5f45bce3af264
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-10-30 16:09:42 -0300

    feat: add functionality to calculate and display average of four grades

---
+++ atividades-praticas/atividade-1/ex2.js
@@ -0,0 +1,10 @@
+const read = require('readline-sync');
+
+let nota1 = read.questionFloat("Digite a nota 1: ", {limitMessage: 'Digite um numero float'})
+let nota2 = read.questionFloat("Digite a nota 2: ", {limitMessage: 'Digite um numero float'})
+let nota3 = read.questionFloat("Digite a nota 3: ", {limitMessage: 'Digite um numero float'})
+let nota4 = read.questionFloat("Digite a nota 4: ", {limitMessage: 'Digite um numero float'})
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log(`Media final: ${media.toFixed(1)}`)
