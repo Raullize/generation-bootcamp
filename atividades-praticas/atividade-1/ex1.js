240994183f6989091b4336b32e3affda465bfc36
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-10-30 16:21:34 -0300

    fix: add missing newline at the end of the file

--- atividades-praticas/atividade-1/ex1.js
+++ atividades-praticas/atividade-1/ex1.js
@@ -2,6 +2,7 @@ const read = require('readline-sync')
 
 let salario = read.questionFloat("Digite o salario: ", {limitMessage: 'Digite um numero float'})
 let abono = read.questionFloat("Digite o abono: ", {limitMessage: 'Digite um numero float'})
+
 let novoSalario = salario + abono
 
 console.log(`Novo Salario: ${novoSalario.toFixed}`)
