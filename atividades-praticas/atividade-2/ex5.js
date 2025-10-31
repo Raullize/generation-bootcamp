80b4fbf538a9365eb2f37d5465309c19fe36cc33
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-10-31 11:59:49 -0300

    feat: add product selection and total calculation functionality
    Implement a simple console application that allows users to select a product by code and quantity, then calculates and displays the total price. Includes error handling for invalid product codes.

---
+++ atividades-praticas/atividade-2/ex5.js
@@ -0,0 +1,41 @@
+import readline from "readline-sync"
+
+const codigo = readline.questionInt("Codigo do Produto: ")
+const quantidade = readline.questionInt("Quantidade: ")
+
+let nomeProduto, precoUnitario
+
+switch (codigo) {
+    case 1:
+        nomeProduto = "Cachorro Quente"
+        precoUnitario = 10.00
+        break
+    case 2:
+        nomeProduto = "X-Salada"
+        precoUnitario = 15.00
+        break
+    case 3:
+        nomeProduto = "X-Bacon"
+        precoUnitario = 18.00
+        break
+    case 4:
+        nomeProduto = "Bauru"
+        precoUnitario = 12.00
+        break
+    case 5:
+        nomeProduto = "Refrigerante" 
+        precoUnitario = 8.00
+        break
+    case 6:
+        nomeProduto = "Suco de laranja"
+        precoUnitario = 13.00
+        break
+    default:
+        console.log("Opção inválida! O código do produto deve ser entre 1 e 6.")
+        precoUnitario = 0
+}
+
+if (precoUnitario > 0) {
+    const valorTotal = quantidade * precoUnitario;
+    console.log(`Produto: ${nomeProduto}\nValor total: R$ ${valorTotal.toFixed(2)}`)
+}
