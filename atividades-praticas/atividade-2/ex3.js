d604354ee5fd120187b1bcf6a25d96d81807f40f
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-10-31 11:59:35 -0300

    feat: add blood donor eligibility check script
    Implement a script that checks blood donor eligibility based on age and first donation status. The script prompts for user input and outputs whether the donor is eligible.

---
+++ atividades-praticas/atividade-2/ex3.js
@@ -0,0 +1,19 @@
+import readline from "readline-sync"
+
+let nome = readline.question("Digite o Nome do doador: ")
+let idade = readline.questionInt("Digite a Idade do doador: ")
+let primeiraDoacao = readline.keyInYNStrict("Primeira doacao de sangue? ")
+
+let statusDoador = "não está apto"
+
+if (idade < 18 || idade > 69) {
+    statusDoador = "não está apto"
+} else if (idade >= 18 && idade < 60) {
+    statusDoador = "está apto"
+} else if (idade >= 60 && idade <= 69) {
+    if (!primeiraDoacao) {
+        statusDoador = "está apto"
+    }
+}
+
+console.log(`${nome} ${statusDoador} para doar sangue!`)
