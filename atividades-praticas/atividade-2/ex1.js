eac3af47e78766676df8be73598aaef40879f336
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-10-31 10:49:11 -0300

    feat: add number comparison script with user input
    Implement a script that takes three numbers as input and compares the sum of the first two with the third number. The script provides appropriate feedback based on the comparison result.

---
+++ atividades-praticas/atividade-2/ex1.js
@@ -0,0 +1,16 @@
+import readline from "readline-sync"
+
+let numeroA = readline.questionInt("Digite o numero A: ", {limitMessage: 'Digite um numero inteiro'})
+let numeroB = readline.questionInt("Digite o numero B: ", {limitMessage: 'Digite um numero inteiro'})
+let numeroC = readline.questionInt("Digite o numero C: ", {limitMessage: 'Digite um numero inteiro'})
+
+let somaNumeros = numeroA + numeroB
+
+if (somaNumeros > numeroC) {
+    console.log("A Soma de A + B é Maior do que C")
+} else if (somaNumeros < numeroC) {
+    console.log("A Soma de A + B é Menor do que C")
+} else {
+    console.log("A Soma de A + B é Igual do que C")
+}
+
