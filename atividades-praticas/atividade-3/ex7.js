ccca66f58dda8f4c8c72e816c2735558ca2576d5
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-11-03 21:09:22 -0300

    feat: add number search functionality in array
    Implement a script that allows users to search for a number in a predefined array and displays its position or a not found message. The solution uses readline-sync for user input and a simple loop for searching.

---
+++ atividades-praticas/atividade-3/ex7.js
@@ -0,0 +1,20 @@
+import readline from "readline-sync"
+
+const arr = [2, 5, 1, 3, 4, 9, 7, 8, 10, 6]
+
+let posicao = null
+
+let num = readline.questionInt("Digite o numero que voce deseja encontrar: ")
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+        posicao = i
+        break
+    }
+}
+
+if (posicao === null) {
+    console.log(`O número ${num} não foi encontrado!`)
+} else {
+    console.log(`O número ${num} está localizado na posição: ${posicao}`)
+}
