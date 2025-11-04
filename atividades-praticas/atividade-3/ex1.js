45e1e28771e8d222a9b1ab55238025f1ea52ebd6
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-11-03 21:08:26 -0300

    feat: add number range multiple checker script
    Implement a script that checks for multiples of 3 and 5 within a given number range. The script prompts for start and end numbers, validates the range, and prints matching numbers with a descriptive message.

---
+++ atividades-praticas/atividade-3/ex1.js
@@ -0,0 +1,14 @@
+import readline from "readline-sync"
+
+let firstNum = readline.questionInt("Digite o primeiro numero do intervalo: ")
+let lastNum = readline.questionInt("Digite o ultimo numero do intervalo: ")
+
+if (firstNum > lastNum) {
+    console.log("\nIntervalo invalido!")
+}
+
+for (let i = firstNum; i <= lastNum; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} é multiplo de ${firstNum} e ${lastNum}`)
+    }
+}
