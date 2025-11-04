170b03ce39738173ef17c73e45ceb4654e1da9db
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-11-03 21:08:34 -0300

    feat: add age statistics counter program
    Implement a program that counts people under 21 and over 50 years old

---
+++ atividades-praticas/atividade-3/ex3.js
@@ -0,0 +1,22 @@
+import readline from "readline-sync"
+
+let idade = 0, pessoasMenorQue21 = 0, pessoasMaiorQue50 = 0
+
+while (idade >= 0) {
+    idade = readline.questionInt("Digite uma idade: ")
+    
+    if (idade < 0) {
+        break
+    }
+    
+    if (idade < 21) {
+        pessoasMenorQue21++
+    }
+
+    if (idade > 50) {
+        pessoasMaiorQue50++
+    }
+}
+
+console.log(`Total de pessoas menores de 21 anos: ${pessoasMenorQue21}`)
+console.log(`Total de pessoas maiores de 50 anos: ${pessoasMaiorQue50}`)
